f58d6bbfba0789527d4fcd65661c54c5b6129e7c
ayush931 <ayushkumar9315983@gmail.com>
2025-05-22 21:29:02 +0530

    Linked List 1 completed

--- homework.js
+++ homework.js
@@ -6,3 +6,7 @@ const hashPassword = await bcrypt.hash(password, 10)
 console.log(hashPassword)
 const comparePassword = await bcrypt.compare(password, hashPassword)
 console.log(comparePassword)
+
+// TODO: req.query, req.params, req.body
+// TODO: how to initiate a mongodb, and connect it to the compass
+// TODO: do the comments in each major concept
